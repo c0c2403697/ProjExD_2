8b2b5b7508bf165933a7f76d2304da068594086b
c0c2403697 <c0c2403697@edu.teu.ac.jp>
2025-04-22 15:21:04 +0900

    4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -55,6 +55,11 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # こうかとんRectと爆弾Rectが重なっていたら
+        if kk_rct.colliderect(bb_rct): 
+             print("Game Over")
+             return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
